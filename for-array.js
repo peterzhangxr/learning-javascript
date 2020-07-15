dbfd1381c2b5764c341725d43ca3cb90bdbf8c78
Peter Zhang <peter.zhang.henan@gmail.com>
2020-07-15 13:59:58 +0800

    搞懂for in for of

--- for-array.js
+++ for-array.js
@@ -1,2 +1,14 @@
 var arr = [1, 2]
-arr[10]
+arr[10] = 10
+console.log(arr)
+arr.name = "遍历数组"
+for(var i in arr) {
+  console.log(i)
+}
+
+for (let item of arr) {
+  console.log(item)
+}
+
+// for in 遍历的是索引
+// for of 遍历的是值
